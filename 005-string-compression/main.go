257b6e62d372c537e6d88b62db8556f4bc3a433a
manuelrojas19 <manuelarr99@gmail.com>
2024-06-29 16:28:59 -0600

    String Compression

---
+++ 005-string-compression/main.go
@@ -0,0 +1,59 @@
+/**
+ * Given an array of characters chars, compress it using the following algorithm:
+ * Begin with an empty string s. For each group of consecutive repeating characters in chars:
+ * - If the group's length is 1, append the character to s.
+ * - Otherwise, append the character followed by the group's length.
+ * The compressed string s should not be returned separately, but instead, be stored in the input character array chars.
+ * Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+ * After modifying the input array chars, the function returns the new length of the array.
+ * You must write an algorithm that uses only constant extra space.
+ * See: https://leetcode.com/problems/string-compression/description/
+ */
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func compress(chars []byte) int {
+	// Initialize variables
+	j := 0     // Pointer to current position in result array
+	count := 1 // Count of consecutive characters
+
+	// Iterate through the array
+	for i := 1; i <= len(chars); i++ {
+		// Check if current character is different from previous one or end of array
+		if i == len(chars) || chars[i] != chars[i-1] {
+			// Assign current character to result array
+			chars[j] = chars[i-1]
+			j++
+
+			// Append count if greater than 1
+			if count > 1 {
+				countStr := strconv.Itoa(count) // Convert count to string
+				for _, ch := range countStr {
+					chars[j] = byte(ch) // Append each character of count string
+					j++
+				}
+			}
+
+			// Reset count for new character group
+			count = 1
+		} else {
+			// Increment count for consecutive characters
+			count++
+		}
+	}
+
+	// Return length of compressed array
+	return j
+}
+
+func main() {
+	// Example usage
+	chars := []byte{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c'}
+	newLength := compress(chars)
+	fmt.Println(string(chars[:newLength])) // Output should be "ab10c"
+	fmt.Println(newLength)                 // Output should be the new length
+}
