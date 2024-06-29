ec22fd80e9563c8315b756d66d172db71562bc1e
manuelrojas19 <manuelarr99@gmail.com>
2024-06-29 12:07:42 -0600

    Updating solution

--- 001-unique-binary-search-trees-ii/main.go
+++ 001-unique-binary-search-trees-ii/main.go
@@ -1,45 +1,51 @@
+/**
+ * Given an integer n,
+ * return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n.
+ * Return the answer in any order.
+ * See: https://leetcode.com/problems/unique-binary-search-trees-ii/
+ */
 package main
 
 import (
 	"fmt"
 )
 
-// Node represents a node in a binary search tree.
-type Node struct {
-	Value int
-	Left  *Node
-	Right *Node
+// TreeNode represents a node in a binary search tree.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
 // GenerateTrees generates all structurally unique BSTs for values from start to end.
-func GenerateTrees(start, end int) []*Node {
+func GenerateTrees(start, end int) []*TreeNode {
 	if start > end {
-		return []*Node{}
+		return []*TreeNode{}
 	}
 
-	memory := make(map[string][]*Node)
-	return generateTrees(start, end, memory)
+	memory := make(map[string][]*TreeNode)
+	return helper(start, end, memory)
 }
 
-func generateTrees(start, end int, memory map[string][]*Node) []*Node {
+func helper(start, end int, memory map[string][]*TreeNode) []*TreeNode {
 	key := fmt.Sprintf("%d-%d", start, end)
 
 	if val, exists := memory[key]; exists {
 		return val
 	}
 
-	var nodes []*Node
+	var nodes []*TreeNode
 
 	if start > end {
 		nodes = append(nodes, nil)
 	} else {
 		for root := start; root <= end; root++ {
-			leftNodes := generateTrees(start, root-1, memory)
-			rightNodes := generateTrees(root+1, end, memory)
+			leftNodes := helper(start, root-1, memory)
+			rightNodes := helper(root+1, end, memory)
 
 			for _, leftNode := range leftNodes {
 				for _, rightNode := range rightNodes {
-					node := &Node{Value: root}
+					node := &TreeNode{Val: root}
 					node.Left = leftNode
 					node.Right = rightNode
 					nodes = append(nodes, node)
@@ -62,12 +68,12 @@ func main() {
 }
 
 // Helper function to print the tree
-func printTree(root *Node) {
+func printTree(root *TreeNode) {
 	if root == nil {
 		fmt.Print("null ")
 		return
 	}
-	fmt.Printf("%d ", root.Value)
+	fmt.Printf("%d ", root.Val)
 	printTree(root.Left)
 	printTree(root.Right)
 }
