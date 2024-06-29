f515cfd71fa265927479a70378786a1f29af0fd5
manuelrojas19 <manuelarr99@gmail.com>
2024-06-29 12:09:19 -0600

    Updating solution

--- 001-unique-binary-search-trees-ii/main.go
+++ 001-unique-binary-search-trees-ii/main.go
@@ -1,6 +1,6 @@
 /**
  * Given an integer n,
- * return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n.
+ * Return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n.
  * Return the answer in any order.
  * See: https://leetcode.com/problems/unique-binary-search-trees-ii/
  */
