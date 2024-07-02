a7967de023bd2e3b1aca1300c18932ac96866d75
manuelrojas19 <manuelarr99@gmail.com>
2024-07-01 23:02:52 -0600

    Merge two sorted list iterative solution

--- 006-merge-two-sorted-list/main.go
+++ 006-merge-two-sorted-list/main.go
@@ -11,15 +11,15 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+func mergeTwoListsRecursive(list1 *ListNode, list2 *ListNode) *ListNode {
 
 	if list1 != nil && list2 != nil {
 
 		if list1.Val < list2.Val {
-			list1.Next = mergeTwoLists(list1.Next, list2)
+			list1.Next = mergeTwoListsRecursive(list1.Next, list2)
 			return list1
 		}
-		list2.Next = mergeTwoLists(list2.Next, list1)
+		list2.Next = mergeTwoListsRecursive(list2.Next, list1)
 		return list2
 	}
 
@@ -27,4 +27,31 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 		return list2
 	}
 	return list1
+}
+
+func mergeTwoListsIterative(list1 *ListNode, list2 *ListNode) *ListNode {
+
+	dummyHead := &ListNode{}
+	currentNode := dummyHead
+
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			currentNode.Next = list1
+			list1 = list1.Next
+		} else {
+			currentNode.Next = list2
+			list2 = list2.Next
+		}
+		currentNode = currentNode.Next
+	}
+
+	// Append the remaining nodes of list1 or list2 (if any)
+	if list1 != nil {
+		currentNode.Next = list1
+	} else {
+		currentNode.Next = list2
+	}
+
+	// Return the head of the merged list (skip the dummy node)
+	return dummyHead.Next
 }
